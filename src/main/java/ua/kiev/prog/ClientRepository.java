38708f35539d5e77748e09dfe765a4cf432b1558
AnnaIsakova <tamgdepesok666@gmail.com>
2017-10-12 19:40:18 +0300

    find all query edited

--- src/main/java/ua/kiev/prog/ClientRepository.java
+++ src/main/java/ua/kiev/prog/ClientRepository.java
@@ -3,8 +3,14 @@ package ua.kiev.prog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
     @Query("SELECT c FROM Client c WHERE c.phone = :phone")
     Client findByPhone(String phone);
+
+    @Override
+    @Query("SELECT c FROM Client c WHERE c.isDeleted = false")
+    List<Client> findAll();
 }
