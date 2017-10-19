05e3d8ba9304540e6a3f29c67bc52b9018e2fecc
AnnaIsakova <tamgdepesok666@gmail.com>
2017-10-19 20:42:44 +0300

    added pagination template

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -38,3 +38,22 @@ function validatePasswords() {
 		confirmPsw.setCustomValidity('');
 	}
 }
+
+function dropUserBtn() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.username-btn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
